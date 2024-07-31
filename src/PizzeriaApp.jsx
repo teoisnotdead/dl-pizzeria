5bbed9bf04ff6009e034d18f476735791f5cb727
TEO <alfredo.ssm@gmail.com>
2024-07-31 10:45:43 -0400

    Se integra tailwind

---
+++ src/PizzeriaApp.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+export function PizzeriaApp() {
+
+  return (
+    <>
+      
+      <h1 className="text-3xl font-bold underline">
+      Hola! Bienvenido a la Pizzeria! 🍕
+    </h1>
+
+    </>
+  )
+}
