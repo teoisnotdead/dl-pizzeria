1eb36d5fbcdabc9a475676333d2b31c4e3419bdc
TEO <alfredo.ssm@gmail.com>
2024-08-27 00:40:08 -0400

    Se agrega boton cerrar sesion a profile

--- front/src/pages/Profile.jsx
+++ front/src/pages/Profile.jsx
@@ -1,3 +1,5 @@
+import { NavLink } from 'react-router-dom'
+
 export const Profile = () => {
   return (
     <>
@@ -5,17 +7,21 @@ export const Profile = () => {
         <h1 className='text-4xl font-medium my-4'>¡Bienvenido a tu perfil!</h1>
         <div className='w-5/6 border-t-[0.1px] border-gray-300 my-5'></div>
         <div className='w-5/6'>
-            <h2 className='text-2xl font-medium mb-3'>Datos personales</h2>
-            <p className='text-lg'>
-              <strong>Nombre: </strong>Alfredo Saavedra
-            </p>
-            <p className='text-lg'>
-              <strong>Email: </strong>
-              <a href='mailto:correo@correo.cl' className='text-blue-500'>
-                correo@correo.cl
-              </a>
-            </p>
+          <h2 className='text-2xl font-medium mb-3'>Datos personales</h2>
+          <p className='text-lg'>
+            <strong>Nombre: </strong>Alfredo Saavedra
+          </p>
+          <p className='text-lg'>
+            <strong>Email: </strong>
+            <a href='mailto:correo@correo.cl' className='text-blue-500'>
+              correo@correo.cl
+            </a>
+          </p>
         </div>
+        <div className='w-5/6 border-t-[0.1px] border-gray-300 my-5'></div>
+        <button className='bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600'>
+          Cerrar sesión
+        </button>
       </div>
     </>
   )
